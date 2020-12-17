687442a1bc1f37db95626b07f9c215dacc8afded
YutoKase <ykase@mizuno.co.jp>
2020-12-17 12:51:19 +0900

    Initial Commit

---
+++ IdentifyingBallTrajectoriesinVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdentifyingBallTrajectoriesinVideo
+//
+//  Created by AM1820 on 2020/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
